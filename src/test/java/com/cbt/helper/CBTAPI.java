dc45adbb85fa9530a3a61770c70559a6c254f078
Kyle Connelly <kyleconnelly@Kyles-MacBook-Pro.local>
2018-08-13 14:29:24 -0500

    we added a set score functionality

---
+++ src/test/java/com/cbt/helper/CBTAPI.java
@@ -0,0 +1,25 @@
+package test.java.com.cbt.helper;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+public class CBTAPI {
+    private String username, authkey;
+
+    public CBTAPI(String username, String authkey) {
+        this.username = username;
+        this.authkey = authkey;
+    }
+
+    public void setScore(String score, String seleniumSessionId) throws Exception {
+        HttpResponse<JsonNode> response = Unirest.put("http://crossbrowsertesting.com/api/v3/selenium/{seleniumSessionId}")
+                                                        .basicAuth(username, authkey)
+                                                        .routeParam("seleniumSessionId", seleniumSessionId)
+                                                        .field("action","set_score")
+                                                        .field("score", score)
+                                                        .asJson();
+    }
+    
+}
--- src/test/java/com/cbt/helper/CBTTest.java
+++ src/test/java/com/cbt/helper/CBTTest.java
@@ -5,30 +5,34 @@ import java.net.URL;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.After;
-
+import org.junit.Assert;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class CBTTest {
-    RemoteWebDriver driver = null;
-    
+    private RemoteWebDriver driver;
+    private CBTAPI api;
+    private String score;
+
     @Before
     public void setUp() throws Exception {
         String username = System.getenv("CBTUSRNAME").replaceAll("@", "%40");
         String authkey = System.getenv("CBTAUTH");
         System.out.println(username);
 		
-		DesiredCapabilities caps = new DesiredCapabilities();
+        DesiredCapabilities caps = new DesiredCapabilities();
 		caps.setCapability("name", "CBT Java re-write");
 		caps.setCapability("browserName", "Internet Explorer");
 		caps.setCapability("version", "10"); // If this cap isn't specified, it will just get the latest one
 		caps.setCapability("platform", "Windows 7 64-Bit");
         caps.setCapability("screenResolution", "1366x768");
-        caps.setCapability("record_video", "true");       
+        caps.setCapability("record_video", "true");     
+        
+        api = new CBTAPI(username, authkey);
 
         String hubAddress = String.format("http://%s:%s@hub.crossbrowsertesting.com:80/wd/hub", username, authkey);
         URL url = new URL(hubAddress);
-
+        score = "fail";
         //use String.format()
         driver = new RemoteWebDriver(url, caps);
     }
@@ -36,15 +40,17 @@ public class CBTTest {
     @Test
     public void testToDo(){
         driver.get("https://www.crossbrowsertesting.com");  
-        if (driver.getTitle().equals("Cross Browser Testing Tool: 1500+ Real Browsers & Devices")) {
-            System.out.println("Our test passed!!!!");
-        }
+
+        Assert.assertEquals("Cross Browser Testing Tool: 1500+ Real Browsers & Devices",driver.getTitle());
+        score = "pass";
     }
  
     @After
-    public void tearDown(){
-        if (driver != null) 
+    public void tearDown() throws Exception{
+        if (driver != null) {
+        api.setScore(score, driver.getSessionId().toString());
             driver.quit();
             //put the set score statement here.
+        }
     }
 }
