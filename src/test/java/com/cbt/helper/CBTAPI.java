955f6dce2afedbc4a30f45f7e80fa9d706227ad0
Kyle Connelly <kyleconnelly@Kyles-MacBook-Pro.local>
2018-08-21 14:56:52 -0500

    edits to remove warnings in cbtapi.java

--- src/test/java/com/cbt/helper/CBTAPI.java
+++ src/test/java/com/cbt/helper/CBTAPI.java
@@ -13,13 +13,13 @@ public class CBTAPI {
         this.authkey = authkey;
     }
 
-    public void setScore(String score, String seleniumSessionId) throws Exception {
+    public void setScore(String score, String seleniumSessionId) throws UnirestException {
         HttpResponse<JsonNode> response = Unirest.put("http://crossbrowsertesting.com/api/v3/selenium/{seleniumSessionId}")
                                                         .basicAuth(username, authkey)
                                                         .routeParam("seleniumSessionId", seleniumSessionId)
                                                         .field("action","set_score")
                                                         .field("score", score)
-                                                        .asJson();
+                                                        .asJson();                                           
     }
     
 }
