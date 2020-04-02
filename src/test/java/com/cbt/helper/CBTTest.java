0241f730782c11e24bfdfbf03bf6f0e5dfc7856f
Kyle Connelly <kyleconnelly@Kyles-MacBook-Pro.local>
2020-04-02 09:17:20 -0500

    saving working test

--- src/test/java/com/cbt/helper/CBTTest.java
+++ src/test/java/com/cbt/helper/CBTTest.java
@@ -14,29 +14,18 @@ import org.openqa.selenium.By;//used if we use the .By in the find elem
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
-
+import com.applitools.eyes.selenium.fluent.Target;
 import com.crossbrowsertesting.AutomatedTest;
 import com.crossbrowsertesting.Builders;
 
-import com.applitools.eyes.selenium.Eyes;
-import com.applitools.eyes.RectangleSize;//used for desktop sizing in Applitools
-
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.openqa.selenium.interactions.Actions;
 
 public class CBTTest {
     private RemoteWebDriver driver;
     private CBTAPI api;
     private String score;
-    private Eyes eyes;
-
-    public void scroll() throws Exception {
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0, 250)");
-
-    }
 
     @Before
     public void setUp() throws Exception {
@@ -45,104 +34,53 @@ public class CBTTest {
         System.out.println(username);
 
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("name", "Make it work");
+        caps.setCapability("name", "Modal Button");
         caps.setCapability("build", "1.0");
-        
-        //Mobile
-        // caps.setCapability("browserName", "Chrome");
-        // caps.setCapability("deviceName", "Nexus 6");
-        // caps.setCapability("platformVersion", "5.0");
-        // caps.setCapability("platformName", "Android");
-        // caps.setCapability("deviceOrientation", "portrait");
-
-        //Desktop
         caps.setCapability("browserName", "Chrome");
-        caps.setCapability("version", "74x64");
+        caps.setCapability("version", "80x64");
         caps.setCapability("platform", "Windows 10");
-        //this will affect CBT RES not EYES RES
-        //caps.setCapability("screenResolution", "2560x1920");
+        caps.setCapability("screenResolution", "1366x768");
         caps.setCapability("record_video", "true");
+        caps.setCapability("record_network", "true");
 
         api = new CBTAPI(username, authkey);
-
-        eyes = new Eyes();
-        eyes.setApiKey(System.getenv("APPLITOOLS"));
 
         String hubAddress = String.format("http://%s:%s@hub.crossbrowsertesting.com:80/wd/hub", username, authkey);
         URL url = new URL(hubAddress);
         driver = new RemoteWebDriver(url, caps);
-
-        // record a video using the API instead of the capabilities above. if this and
-        // Caps is on it will record 2 and make it seem like 2 tests happened.
-        // api.record_video(driver.getSessionId().toString());
     }
 
     @Test
     public void testToDo() {
-        // open a new instance of eyes
-        // for desktops
-        eyes.open(driver, "CrossBrowserTesting", "Name of test for EYES");
-        // use if you want to have a specific resolution. 
-        // eyes.open(driver, "CrossBrowserTesting", "Name of test for EYES",new RectangleSize(800, 600));
-        
-
-        // for mobiles. you dont need the RectangleSize()
-        //eyes.open(driver, "Max Window", "Testing for Window Maximize");
 
         // Get Site.
-        driver.get("https://www.google.com");
+        driver.get("https://www.biotrust.com/pages/ancient-remedy");
+        System.out.println("got bio-trust page");
 
-        // Visual checkpoint
-        eyes.checkWindow("Google Search");
-
-        // for desktops.
+        //for desktops.
         driver.manage().window().maximize();
-        // System.out.println("window maximized.");
+        System.out.println("window maximized.");
 
         // Test 1:check what title equals.
-        Assert.assertEquals("Google", driver.getTitle());
+        Assert.assertEquals("Ageless Multi-Collagen Protein Powder Ancient Remedy VL T1", driver.getTitle());
         System.out.println(driver.getTitle());
 
-        // Test 2
-
-        int tries = 0;
-        WebElement button = null;
-        while (tries < 10) {
-            try {
-                button = driver.findElementByCssSelector(
-                        "#hptl > a:nth-child(1)");
-                break;
-            } catch (Exception e) {
-                // System.out.println("error finding elem: " + e.getMessage());
-                try {
-                    scroll();
-
-                } catch (Exception e2) {
-                    System.out.println("error scrolling: " + e2.getMessage());
-                }
-                tries++;
-            }
+        try{
+            Actions action = new Actions(driver);
+            action.moveByOffset(0, 300).perform();
+        } catch (Exception e) {
+            System.out.println("Error:" + e.getMessage());
         }
 
-        if (button != null) {
-            button.click();
-        }
-        WebDriverWait wait = new WebDriverWait(driver, 5);
+        driver.findElement(By.id("ouibounce-modal"));
 
-        WebElement logo = null;
-        logo = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(
-                "body > main > div > div > p")));
-        if (logo != null) {
-            System.out.println("found elem");
-        }
-        Assert.assertEquals("About | Google", driver.getTitle());
-
-        eyes.checkWindow("Our");
-
-        System.out.println(driver.getTitle());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
         
-
-        eyes.close();
+            e.printStackTrace();
+        }
+        
         score = "Pass";
 
         AutomatedTest myTest = new AutomatedTest(driver.getSessionId().toString());
@@ -162,11 +100,8 @@ public class CBTTest {
     public void tearDown() throws Exception {
         if (driver != null) {
 
-            // Set the score depending on the tests.
-            // api.setScore(score, driver.getSessionId().toString());
-
             driver.quit();
-            eyes.abortIfNotClosed();
+            
             System.out.println(score);
         }
     }
